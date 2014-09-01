887756f08e75b405d5b447e83596157140d76a35
Sarah Mitchell <TheSmitchell@users.noreply.github.com>
2014-09-01 14:53:43 -0400

    background
    Adding paper background, I think

--- helper.js
+++ helper.js
@@ -1,6 +1,17 @@
 function format(){
     var b=document.getElementsByTagName("pre");
-    for (i=0;i<b.length;i++)
+    for (i=0;i<b.length;i++) {
         hljs.highlightBlock(b[i],null,false)
+    }
+    
+  var eles = document.getElementsByTagName('pre');
+  for (i = 0; i < eles.length; i++) {
+    var classes = eles[i].className;
+    classes.split(' ');
+    classes.push('paper');
+    classes.join(' ');
+    eles[i].className = classes;
+  }
+
 }
 
